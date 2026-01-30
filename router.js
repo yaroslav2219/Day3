e5539fad9937dd1afc98b3ae38e47723c6923cdf
Yaroslav <kya.pk22.6.3@gmail.com>
2026-01-30 10:47:29 +0200

    Update router.js

--- router.js
+++ router.js
@@ -6,8 +6,19 @@ export const router = VueRouter.createRouter({
     routes: [
         { path: '/', name:'Sign in', component: login },
         { path: '/campaigns', name:'Campaigns', component: campaigns },
+
+          {
+            path: '/campaign/:id',
+            name: 'Campaign view',
+            component: {
+                template: `<div style="padding:20px">
+                    <h2>Campaign {{ $route.params.id }}</h2>
+                </div>`
+            }
+        }
     ]
 });
+
 
 
 
