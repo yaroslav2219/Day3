23c01ef33574da46249bc8993d202544525ca2b3
Yaroslav <kya.pk22.6.3@gmail.com>
2026-01-30 13:49:51 +0200

    Update msg.js

--- msg.js
+++ msg.js
@@ -89,33 +89,25 @@ return new Promise(function(resolve, reject){
 });
         }
     },
-  template: `
-<div>
-
-  <div class="alertMsg" v-if="alert">
+    template: `
+    <div class="alertMsg" v-if="alert">
     <div class="wrapper al">
-      <i class="fas fa-check-circle"></i> {{ alert }}
+    <i class="fas fa-check-cicle"></i> {{alert}}
     </div>
-  </div>
-
-  <div class="successMsg" v-if="success">
+    </div>
+    <div class="successMsg" v-if="success">
     <div class="wrapper al">
-      <i class="fas fa-check-circle"></i> {{ success }}
-    </div>
-  </div>
-
-  <popup ref="confirm" :title="confirmTitle">
-    <div class="al">
-      <i class="fas fa-info-circle"></i> {{ confirm }}
-      <div class="botBtns">
-        <a class="btnS" href="#" @click.prevent="code = 1">Yes</a>
-        <a class="btnS" href="#" @click.prevent="code = 2">No</a>
-      </div>
-    </div>
-  </popup>
-
-</div>
-`
-
-
+     <i class="fas fa-check-cicle"></i> {{success}}
+     </div>
+     </div>
+     <popup ref="confirm" :title="confirmTitle">
+     <div class=" al">
+     <i class="fas fa-info-circle"></i> {{confirm}}
+     <div class="botBtns">
+     <a class="btnS" href="#" @click.prevent="code=1">Yes</a>
+     <a class="btnS" href="#" @click.prevent="code=2">No</a>
+     </div>
+     </div>
+     </popup>
+    `
 };
